6765ec99dfbd33503b871905acf1dfc81ab65374
Mitchell Adair <rmadair88@gmail.com>
2012-09-01 18:45:40 -0500

    v1.0, single process

--- fuzzer.py
+++ fuzzer.py
@@ -1,12 +1,24 @@
 from sys import exit, argv
+from sys import stdout
 from Mutator import Mutator
 from Executor import executor
-from time import ctime
+from time import ctime, time
+from multiprocessing import Pool, Process, Pipe
+from subprocess import Popen
 import threading
 import logging
+import gc
 
-#### XXXXXX if wanting to do 64 bit and 32, just take out all 0x%08x and replace with a global define
-####        ADDR_STRING or something like that...
+# todo
+#
+# - make the to_run string (and the way the user defines where the mutated file goes in the command line) more generic
+#   - fill in with a regex or something
+#
+# - allow a max time for a process to run
+#
+# - use the arg parse library
+#
+# - support multiple processes
 
 def usage():
     print 'Usage : %s [program_cmd_line] [original_file] [temp_directory] [mutation_type] [max_processes] [log_file][save_directory]' % argv[0]
@@ -25,6 +37,7 @@ if __name__ == "__main__":
     if len(argv) != 8:
         usage()
 
+    # parse command line args
     cmd_line        = argv[1]
     original_file   = argv[2]
     temp_directory  = argv[3]
@@ -33,7 +46,11 @@ if __name__ == "__main__":
     logfile_name    = argv[6]
     save_directory  = argv[7]
 
+    # typecast, create the worker pool
+    max_processes   = int(max_processes)
+    pool            = Pool(max_processes, maxtasksperchild=50)
     
+    # log basic starting information
     logging.basicConfig(filename=logfile_name, level=logging.INFO)
     logging.info("Starting Fuzzer")
     logging.info("%s" % ctime())
@@ -41,25 +58,39 @@ if __name__ == "__main__":
     logging.info('original_file   = %s' % original_file)
     logging.info('temp_director   = %s' % temp_directory)
     logging.info('mutation_type   = %s' % mutation_type)
-    logging.info('max_processes   = %s' % max_processes)
+    logging.info('max_processes   = %d' % max_processes)
     logging.info('logfile_name    = %s' % logfile_name)
     logging.info('save_directory  = %s' % save_directory)
 
+    # create the mutator, and log the possible values it will produce
     mutator = Mutator(original_file, temp_directory, mutation_type)    
     logging.info('total mutations = %d' % mutator.total_mutations)
     logging.info('possible mutation list :')
     for offset, mutation in enumerate(mutator.getValues()):
         logging.info('[%02d] : %s'% (offset, repr(mutation)) )
 
-    counter = 0
-    for (offset, value_index, value_type, new_file) in mutator.createNext():
-        if counter==50:
-            break
+    # figure out some relative percentages
+    ten_percent = mutator.total_mutations / 10
+    percent = 0
+
+    # the main loop - yield each mutation, execute and log it
+    start_time = time()
+    for counter, (offset, value_index, value_type, new_file) in enumerate(mutator.createNext()):
+
+        # just for sanity
+        if counter % ten_percent == 0:
+            print '%02d%% - %s' % (percent, ctime())
+            percent += 10
+
         torun = '%s %s' % (cmd_line, new_file)
         logger=logging.getLogger('Executor-%d'%counter)
-        thread = threading.Thread(target=executor, args=(torun,offset,value_index,new_file,counter,save_directory,logger))
-        thread.run()
-        while thread.isAlive():
-            continue
-        counter += 1
+        # awesome, but creates the files up front :( - pool.apply_async(executor, (torun,offset,value_index,new_file,counter,save_directory,0), callback=output_logging_callback)#logger))
+        res = pool.apply(executor, (torun,offset,value_index,new_file,counter,save_directory))
+        logging.info(res)
+
+    # clean up and wait
+    pool.close()
+    pool.join()
     logging.info("test finished")
+    end_time = time() - start_time
+    print 'total time =', end_time
